868770c25a94164981a1839f358555d28a816170
Sasan <sasanparviz1998@gmail.com>
2025-11-26 15:42:06 +0330

    feat(swagger): added support for swagger

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -12,6 +13,15 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Server API')
+    .setDescription('The server API description')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
