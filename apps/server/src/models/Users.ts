a8bc5692c9d9b466e41dee4466b69f008b0a532d
Sasan <sasanparviz1998@gmail.com>
2025-12-02 14:51:16 +0330

    feat: added Users Model

---
+++ apps/server/src/models/Users.ts
@@ -0,0 +1,65 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Index,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { WorkingReports } from './WorkingReports';
+
+@Index('PK_users', ['id'], { unique: true })
+@Entity('users', { schema: 'dbo' })
+export class Users {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id!: number;
+
+  @Column('int', { name: 'employeeId', unique: true })
+  employeeId!: number;
+
+  @Column('nvarchar', { name: 'RFID', nullable: true, length: 50 })
+  rfid!: string | null;
+
+  @Column('nvarchar', { name: 'personName', length: 128 })
+  personName!: string;
+
+  @Column('nvarchar', { name: 'personFamily', length: 128 })
+  personFamily!: string;
+
+  @Column('nvarchar', { name: 'personFullName', length: 256 })
+  personFullName!: string;
+
+  @Column('nvarchar', { name: 'organizationName', nullable: true, length: 50 })
+  organizationName!: string | null;
+
+  @Column('nvarchar', {
+    name: 'organizationUnitName',
+    nullable: true,
+    length: 50,
+  })
+  organizationUnitName!: string | null;
+
+  @Column('nvarchar', {
+    name: 'organizationalPostCaption',
+    nullable: true,
+    length: 50,
+  })
+  organizationalPostCaption!: string | null;
+
+  @CreateDateColumn({
+    type: 'datetime2',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdAt!: Date;
+
+  @UpdateDateColumn({
+    type: 'datetime2',
+    default: () => 'CURRENT_TIMESTAMP',
+    onUpdate: 'CURRENT_TIMESTAMP',
+  })
+  updatedAt!: Date;
+
+  @OneToMany(() => WorkingReports, (workingReports) => workingReports.user)
+  workingReports!: WorkingReports[];
+}
